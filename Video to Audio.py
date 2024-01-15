6229dd3685021339e5a41b5a4eedfe7b1fe93ac6
Kunal Chauhan <119841721+kunalchauhan69@users.noreply.github.com>
2024-01-15 19:33:22 +0530

    Update Video to Audio.py

--- Video to Audio.py
+++ Video to Audio.py
@@ -1,30 +1,29 @@
 from pytube import YouTube
-import pytube
 import os
 
 def main():
-     video_url = input('Enter YouTube video url')
+    video_url = input('Enter the YouTube video URL: ')
 
-     if os.name == 'nt':
-         path = os.getcwd() + '\\'
+    if os.name == 'nt':
+        path = os.getcwd() + '\\'
+    else:
+        path = os.getcwd() + '/'
 
-     else:
-         path = os.getcwd() + '/'
+    try:
+        yt = YouTube(video_url)
+        video_stream = yt.streams.filter(only_audio=True).first()
+        video_stream.download(output_path=path)
 
-         name = pytube.extract.video_id(video_url)
-         YouTube(video_url).streams.filter(only_audio=True).first().download(filename=name)
-         location = path + name + '.mp4'
-         renametomp3 = path + name + '.mp3'
+        name = yt.title.replace(" ", "_")
+        location = path + name + '.mp4'
+        renametomp3 = path + name + '.mp3'
 
-     if os.name == 'nt':
-         os.system('ren {0} {1}'. format(location, renametomp3))
+        os.rename(location, renametomp3)
 
-     else: 
-         os.system('mv {0} {1}'. format(location, renametomp3))
+        print(f'Download and conversion completed: {renametomp3}')
 
-         if __name__ == '__main__':
-                  main()
+    except Exception as e:
+        print(f'Error: {e}')
 
-
-     
- 
+if _name_ == '_main_':
+    main()
